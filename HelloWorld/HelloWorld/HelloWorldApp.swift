463ac20e005faa08ee31f07d653de6db571e6b83
SY <66587837+Seon-Yeong-Lee@users.noreply.github.com>
2021-02-24 00:04:37 +0900

    Add iOS HelloWorld
    Run HelloWorld

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by SY Lee on 2021/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
